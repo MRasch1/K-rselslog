9b0575138d4767122ae03682a4309f6a87b3cbe3
Magnus Aleksander Bilsted Rasch <mrasch1102@gmail.com>
2023-01-17 13:57:16 +0100

    RelationsQuery

--- SQLQuery3.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[BilData] (
-    [Bil_ID]      INT          IDENTITY (1, 1) NOT NULL,
-    [Navn]    VARCHAR (20) NULL,
-    [NrPlade] VARCHAR (20) NULL,
-    [Dato]    DATETIME     NULL,
-    PRIMARY KEY CLUSTERED ([Bil_ID] ASC)
-);
-
